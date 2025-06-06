e8d279830ab109f71cfee21ceb1cf7d8ce7e6104
jnors <jnors@ua.pt>
2025-06-06 16:52:24 +0100

    Updated login page to include Suspense

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,9 +1,12 @@
-import LoginForm from '../../components/LoginForm';
+import { Suspense } from "react";
+import LoginForm from "../../components/LoginForm";
 
 export default function LoginPage() {
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-50">
-      <LoginForm />
+      <Suspense fallback={<div>Loading...</div>}>
+        <LoginForm />
+      </Suspense>
     </div>
   );
 }
